bb882b72bd219f8713b9d3120d34e0c6a5206a8f
levsmertin <ulanov@psu.ru>
2017-06-15 16:10:30 +0600

    kt2.3

--- 4_AES/4_AES/4_AES.cpp
+++ 4_AES/4_AES/4_AES.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include "iostream"
 #include <string>
 #include "..\..\cryptopp\aes.h"
@@ -230,7 +229,7 @@ private:
 };
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
 	std::string plaintext_file;
 	std::string key_file;
